ee435344da062360d5aa51b5dff7eb02d8eac16c
Prakasa <prakasa@devetek.com>
2025-08-29 14:13:48 +0700

    style: test logrotate

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Elysia } from "elysia";
 
-const {PORT = 3000 } = process.env;
-const app = new Elysia().get("/", ({ set }) => {
-  set.headers = { 'Content-Type': 'text/html; charset=utf-8' }
+const { PORT = 3000 } = process.env;
+const app = new Elysia()
+  .get("/", ({ set }) => {
+    set.headers = { "Content-Type": "text/html; charset=utf-8" };
 
-  return "Hello from Elysia deploy with dPanel. Visit <a href='https://cloud.terpusat.com/'>dPanel</a> to deploy your Javascript application build and run with Bun.";
-}).listen(PORT);
+    return "Hello from Elysia deploy with dPanel. Visit <a href='https://cloud.terpusat.com/'>dPanel</a> to deploy your Javascript application build and run with Bun.";
+  })
+  .listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+setInterval(() => {
+  console.error("Hello from Elysia deploy with dPanel. Visit <a href='https://cloud.terpusat.com/'>dPanel</a> to deploy your Javascript application build and run with Bun. This message from interval to test logrotate!")
+}, 1000);
